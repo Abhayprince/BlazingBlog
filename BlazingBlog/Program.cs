26afef0f58f45533ff3d4daf982ceded816fc4e6
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-06-06 00:20:34 +0530

    Part-2 Layout and TopNavBar implementation. Cleanup by removing all the files came with default blazor project template

--- BlazingBlog/Program.cs
+++ BlazingBlog/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var blogConnectionString = builder.Configuration.GetConnectionString("Blog");
 
